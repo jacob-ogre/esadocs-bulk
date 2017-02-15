9ac746be5b9f7bba86c9444dd2510f4ff9b72055
Jacob Malcom <jmalcom@defenders.org>
2017-02-15 14:49:13 -0500

    Add chmod, correct df construction for accession.

--- inst/auto_accession.R
+++ inst/auto_accession.R
@@ -4,12 +4,14 @@
 # This script lives on and is run on the DOC_SERVER side.
 
 library(digest)
+library(dplyr)
 library(elastic)
 library(pdftools)
 
 readRenviron("/home/jacobmalcom/.Renviron")
 
 DOC_SERVER <- Sys.getenv("DOC_SERVER")
+DOC_PATH <- Sys.getenv("DOC_PATH")
 STG_PATH <- Sys.getenv("STG_PATH")
 
 infiles <- list.files(
@@ -27,7 +29,6 @@ rand_str <- function(len=30) {
   return(str)
 }
 
-
 accession_file <- function(f) {
   raw_txt <- unlist(pdf_text(f))
   pdf_md5 <- digest::digest(f, file = TRUE)
@@ -38,38 +39,32 @@ accession_file <- function(f) {
     pattern = "/home/jacobmalcom/Data",
     replacement = "https://esadocs.cci-dev.org"
   )
+  doc_id <- rand_str()
   body <- list(
     raw_txt = raw_txt,
     pdf_path = pdf_path,
     pdf_md5 = pdf_md5
   )
-  print(paste("type:", type))
-  print("body:")
-  print(body)
-  # acc_f <- docs_create(
-  #   index = "esadocs",
-  #   type = type,
-  #   id = rand_str(),
-  #   body = body
-  # )
-  # rn <- file.rename(f, newf)
-  rn <- TRUE
+  acc_f <- docs_create(
+    index = "esadocs",
+    type = type,
+    id = doc_id,
+    body = body
+  )
+  rn <- file.rename(f, newf)
+  chmod <- Sys.chmod(newf, mode = "0644")
   return(list(acc_res = acc_f$result,
-              mv_res = rn))
+              mv_res = rn,
+              doc_id = doc_id))
 }
 
 connect()
-acc_res <- unlist(lapply(infiles, accession_file))
-acc_res <- data.frame(
-  file <- infiles,
-  acc_res <- acc_res$acc_res,
-  mv_res <- acc_res$mv_res,
-  stringsAsFactors = FALSE
-)
+acc_res <- bind_rows(lapply(infiles, accession_file))
+acc_res$file <- infiles
 
 save(acc_res,
      file = file.path(
-       BULK_PATH, "rda",
+       DOC_PATH, "rda",
        paste0("accession_result_",
               gsub(Sys.time(), pattern = " |:", replacement = "_"),
               ".rda")
